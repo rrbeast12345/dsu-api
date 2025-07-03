2ea30b1b96c5efeac77293e63ece1eb5408f7612
oliver gardi <demonicentityz8@gmail.com>
2025-07-03 14:40:47 +0200

    backend

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ import time
 # users = {'231':["231", "Thandi Mkhize", "1976-01-01", True],'341':["341", "oliver gardi", "2008-05-23", False]}
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 BASE_DIR = Path(__file__).resolve().parent
-app.mount("/", StaticFiles(directory=str(BASE_DIR / "frontend for replit"), html=True), name="static")
+app.mount("/", StaticFiles(directory=str(BASE_DIR / "frontend for replit"), html=True), name="frontend for replit")
 def timestamp():
     return datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
 
