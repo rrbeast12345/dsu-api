ecf7c32616d14601c96454ad49dffb2e369c9d51
oliver gardi <demonicentityz8@gmail.com>
2025-07-03 14:43:16 +0200

    backend

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ from datetime import datetime, timezone
 from fastapi.middleware.cors import CORSMiddleware
 import shutil
 from pathlib import Path
-from fastapi.templating import Jinja2Templates
+
 
 
 
@@ -19,8 +19,10 @@ app = FastAPI()
 import time
 # users = {'231':["231", "Thandi Mkhize", "1976-01-01", True],'341':["341", "oliver gardi", "2008-05-23", False]}
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
 BASE_DIR = Path(__file__).resolve().parent
-app.mount("/", StaticFiles(directory=str(BASE_DIR / "frontend for replit"), html=True), name="frontend for replit")
+app.mount("/frontend", StaticFiles(directory=str(BASE_DIR / "frontend for replit"), html=True), name="frontend for replit")
+
 def timestamp():
     return datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
 
