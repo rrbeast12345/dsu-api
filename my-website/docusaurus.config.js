b13f0a35816818d88b5de2ec53aafd1c6735131d
oliver gardi <demonicentityz8@gmail.com>
2025-07-08 12:17:42 +0200

    frontend

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'http://127.0.0.1:8000/documentation/index.html',
+  url: 'http://127.0.0.1:8000',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/documentation/',
